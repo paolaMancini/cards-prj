7b33e5056ca11505d473d5ee40c120ffe23c5ce7
Mancini Paola <mancinip@corp.dom>
2019-12-20 14:51:37 +0100

    Solved issue

--- features/webex_card_feedback.js
+++ features/webex_card_feedback.js
@@ -4,7 +4,7 @@
 module.exports = function (controller) {
 
     controller.adapter.registerAdaptiveCardWebhookSubscription(controller.getConfig('webhook_uri'));
-    controller.hears('xmasc-card', 'message,direct_message', async (bot, message) => {
+    controller.hears('xmas_card', 'message,direct_message', async (bot, message) => {
         //  await bot.reply(message, "Please, compile the following feedback card");
         await bot.reply(message, {
             text: 'Best Wishes from Paola Mancini',
