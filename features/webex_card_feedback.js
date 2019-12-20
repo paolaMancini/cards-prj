cbdeeac3f05d6b8cce9741666ffffd16e3fa935f
Mancini Paola <mancinip@corp.dom>
2019-12-20 15:56:14 +0100

    ok

--- features/webex_card_feedback.js
+++ features/webex_card_feedback.js
@@ -4,75 +4,81 @@
 module.exports = function (controller) {
 
     controller.adapter.registerAdaptiveCardWebhookSubscription(controller.getConfig('webhook_uri'));
+
     controller.hears('xmas_card', 'message,direct_message', async (bot, message) => {
         //  await bot.reply(message, "Please, compile the following feedback card");
-        await bot.reply(message, {
-            text: 'Best Wishes from Paola Mancini',
-            attachments: [
-                {
-                    "contentType": "application/vnd.microsoft.card.adaptive",
-                    "content": {
-                        "type": "AdaptiveCard",
-                        "version": "1.0",
-                        "body": [
-                            {
-                                "type": "TextBlock",
-                                "text": "Christmas Card Messages by Paola Mancini, leveraging  Webex Teams Cards & Buttons",
-                                "color": "Accent",
-                                "wrap": true,
-                                "size": "Medium",
-                                "weight": "Bolder"
-                            },
-                            {
-                                "type": "Image",
-                                "url": "https://i.pinimg.com/736x/17/9c/fc/179cfcd504475b3df212495cf9012e0e.jpg",
-                                "size": "auto"
-                            },
-                            {
-                                "type": "ColumnSet",
-                                "columns": [
-                                    {
-                                        "type": "Column",
-                                        "width": 2,
-                                        "items": [
-                                            {
-                                                "type": "TextBlock",
-                                                "text": "May this Christmas fill your heart with warmth and love. I wish you  joy and serendipity for the new year!",
-                                                "weight": "Bolder",
-                                                "size": "Medium",
-                                                "horizontalAlignment": "Left",
-                                                "wrap": true,
-                                                "spacing": "Small"
-                                            },
-                                            {
-                                                "type": "TextBlock",
-                                                "text": "Leave your whishes for Paola",
-                                                "wrap": true
-                                            },
-                                            {
-                                                "type": "Input.Text",
-                                                "id": "myName",
-                                                "placeholder": "..."
-                                            }
-                                        ]
-                                    }
-                                ]
-                            }
-                        ],
-                        "actions": [
-                            {
-                                "type": "Action.Submit",
-                                "title": "Send"
-                            }
-                        ]
+        let from = await bot.api.people.get(message.actorId);
+        console.log("Message from: ", person);
+
+        if (from === 'Paola Mancini') {
+            await bot.reply(message, {
+                text: 'Best Wishes from Paola Mancini',
+                attachments: [
+                    {
+                        "contentType": "application/vnd.microsoft.card.adaptive",
+                        "content": {
+                            "type": "AdaptiveCard",
+                            "version": "1.0",
+                            "body": [
+                                {
+                                    "type": "TextBlock",
+                                    "text": "Christmas Card Messages by Paola Mancini, leveraging  Webex Teams Cards & Buttons",
+                                    "color": "Accent",
+                                    "wrap": true,
+                                    "size": "Medium",
+                                    "weight": "Bolder"
+                                },
+                                {
+                                    "type": "Image",
+                                    "url": "https://i.pinimg.com/736x/17/9c/fc/179cfcd504475b3df212495cf9012e0e.jpg",
+                                    "size": "auto"
+                                },
+                                {
+                                    "type": "ColumnSet",
+                                    "columns": [
+                                        {
+                                            "type": "Column",
+                                            "width": 2,
+                                            "items": [
+                                                {
+                                                    "type": "TextBlock",
+                                                    "text": "May this Christmas fill your heart with warmth and love. I wish you  joy and serendipity for the new year!",
+                                                    "weight": "Bolder",
+                                                    "size": "Medium",
+                                                    "horizontalAlignment": "Left",
+                                                    "wrap": true,
+                                                    "spacing": "Small"
+                                                },
+                                                {
+                                                    "type": "TextBlock",
+                                                    "text": "Leave your whishes for Paola",
+                                                    "wrap": true
+                                                },
+                                                {
+                                                    "type": "Input.Text",
+                                                    "id": "myName",
+                                                    "placeholder": "..."
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ],
+                            "actions": [
+                                {
+                                    "type": "Action.Submit",
+                                    "title": "Send"
+                                }
+                            ]
+                        }
                     }
-                }
-            ]
-        })
+                ]
+            })
+        } else {
+            await bot.reply(message, 'Sorry! Only Paola is authorized to ask for xmas_card!');
+        }
+
     })
-
-
-
 
 
     controller.on('attachmentActions', async (bot, message) => {
@@ -81,14 +87,14 @@ module.exports = function (controller) {
         //  console.log("MESSAGE from: " ,message.personId);
         //console.log("message: ",message.inputs);
 
-        let from = message.incoming_message.from.id;
-        var personId = message.personId;
-        console.log("========> from: " + from);
-        console.log("========> personId: " + personId);
+        //let from = message.incoming_message.from.id;
+        //var personId = message.personId;
+        //console.log("========> from: " + from);
+        //console.log("========> personId: " + personId);
 
         //get person name
         let person = await bot.api.people.get(message.actorId);
-        console.log("$$$$$$$$ person: ", person);
+        console.log("Message from: ", person);
 
 
         let reply = await bot.reply(message, {
@@ -120,8 +126,6 @@ module.exports = function (controller) {
 
 
     })
-
-
 
 
 
