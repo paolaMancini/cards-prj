20d26ac89bfe3e54bf4dc766e3aa3b5e3c09106f
paolaMancini <paola.mancini@italtel.com>
2019-12-20 12:22:24 +0100

    Prova delete

--- features/webex_card_feedback.js
+++ features/webex_card_feedback.js
@@ -104,11 +104,12 @@ module.exports = function (controller) {
             ]
         });
         console.log("#### Deleting message= ");
-        setTimeout("Deleting message=", 5000);
-        bot.deleteMessage();
+        // setTimeout("Deleting message=", 5000);
+        await bot.cancelAllDialogs();
+
     })
 
- 
+
     controller.commandHelp.push({ command: 'feedback_card', text: 'Demo interactive adaptive cards' });
 
 }
